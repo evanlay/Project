b13aa5b523502ad60a4ab090a395af9cc46148f1
evanlay <evanlay@billericak12.com>
2019-09-13 11:27:40 -0400

    Code stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Goodbye");
+        //double x = Math.random(); // random number [0,1)
+        //double x = Math.random()*10; // random double [0.0,10.0)
+        //int x = (int)(Math.random()*10 + 10); // random int [10,20)
+        int x = (int)(Math.random()*);
+        //(int)(Math.random() *a+b) // a numbers starting at b
+        System.out.println(x);
     }
 }
